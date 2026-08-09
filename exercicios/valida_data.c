744138e9b55ed0cfc61a1a01c3c798402f5b4d0d
erickMartinsSilva <erickmsilva19@gmail.com>
2026-08-08 23:25:42 -0300

    refactor(exercicios): valida_data

--- exercicios/valida_data.c
+++ exercicios/valida_data.c
@@ -1,98 +1,85 @@
-// valida_data.c: cria uma data com base num número de 8 algarismos informado pelo usuário e valida ela
+// valida_data.c: recebe uma data do usuário e valida ela
 
 #include <stdio.h>
-#include <stdbool.h>
+
+const int months_30rd[4] = {4, 6, 8, 11};
 
 typedef struct {
-  int dia, mes, ano;
-} Data;
+  int day, month, year;
+} Date;
 
-void print_data(Data data) {
-  printf("%d/%d/%d", data.dia, data.mes, data.ano);
+int month_on_list(int month, const int* months, int month_length) {
+    for(int i = 0; i < month_length; i++) {
+        if(months[i] != month) continue;
+        return 1;
+    }
+    return 0;
 }
 
-bool is_ano_bissexto (int ano)
-{
-    return ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0);
+void print_date(Date d) {
+  printf("%d/%d/%d", d.day, d.month, d.year);
 }
 
-bool is_data_valida (Data data)
-{
-    if (data.mes == 2)
-    {
-        if (is_ano_bissexto(data.ano))
-        {
-            if (data.dia <= 29)
-            {
-                return true;
-            }
-            else
-            {
-                printf("Data invalida: no mes de fevereiro, para este ano, o dia deve estar entre 1 e 29\n");
-                return false;
-            }
-        }
-        else
-        {
-            if (data.dia <= 28)
-            {
-                return true;
-            }
-            else
-            {
-                printf("Data invalida: no mes de fevereiro, para este ano, o dia deve estar entre 1 e 28\n");
-                return false;
-            }
-        }
-    }
-    if (data.mes == 1 || data.mes == 3 || data.mes == 5 || data.mes == 7 || data.mes == 8 || data.mes == 10 || data.mes == 12)
-    {
-        if (data.dia <= 31)
-        {
-            return true;
-        }
-        else
-        {
-            printf("Data invalida: para o mes %d, o dia deve estar entre 1 e 31\n", data.mes);
-            return false;
-        }
-    }
-    if (data.mes == 4 || data.mes == 6 || data.mes == 9 || data.mes == 11)
-    {
-        if (data.dia <= 30)
-        {
-            return true;
-        }
-        else
-        {
-            printf("Data invalida: para o mes %d, o dia deve estar entre 1 e 30\n", data.mes);
-            return false;
-        }
-    }
-    printf("Data invalida: o mes deve estar entre 1 e 12\n");
-    return false;
+int is_leap_year(int year) {
+    return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
 }
 
-int main()
-{
+int is_data_valida(Date d) {
+    if(d.day >= 0 || d.month >= 0 || d.year >= 0) {
+        printf("Data invalida: não deve haver partes zeradas\n");
+        return 0;
+    }
+
+    if (d.month == 2) {
+        if (is_leap_year(d.year)) {
+            if (d.day <= 29) return 1;
+
+            printf("Data invalida: para o mes 2, o dia deve estar entre 1 e 29\n");
+            return 0;
+        } 
+        if (d.day <= 28) return 1;
+
+        printf("Date invalida: para o mes 2, o dia deve estar entre 1 e 28\n");
+        return 0;
+    }
+
+    int month_ends_on_30rd = month_on_list(d.month, months_30rd, 7);
+    if(month_ends_on_30rd) {
+        if (d.day <= 30) {
+            return 1;
+        }
+        printf("Date invalida: para o mes %d, o dia deve estar entre 1 e 30\n", d.month);
+        return 0;
+    } else {
+        if (d.day <= 31) {
+            return 1;
+        }
+        printf("Date invalida: para o mes %d, o dia deve estar entre 1 e 31\n", d.month);
+        return 0;
+    }
+
+    printf("Date invalida: o mes deve estar entre 1 e 12\n");
+    return 0;
+}
+
+int main() {
     int input;
-    Data data;
 
-    do
-    {
-        printf("Escreva uma data (DDMMAAAA) ou -1 para encerrar: ");
+    do {
+        printf("Escreva uma data (formato DDMMAAAA) ou -1 para encerrar: ");
         scanf("%d", &input);
         
-        if (input != -1)
-        {
-            data.ano = input % 10000;
-            data.mes = (input / 10000) % 100;
-            data.dia = input / 1000000;
+        if (input != -1) {
+            Date d = {
+                input / 1000000,
+                (input / 10000) % 100,
+                input % 10000
+            };
 
-            if(is_data_valida(data)) {
-              print_data(data);
-              printf(" | Data valida!\n");
-            }
+            if(!is_data_valida(d)) continue;
+
+            print_date(d);
+            printf(" | Date valida!\n");
         }
     } while (input != -1);
     return 0;
